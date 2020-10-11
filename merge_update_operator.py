2dafdf16d19d1248ae7f3e45000cc26a584513c3
jairoufps <jairoandrescp@ufps.edu.co>
2020-10-11 18:59:19 -0500

    added native times feature example and new  dict operators

---
+++ merge_update_operator.py
@@ -0,0 +1,41 @@
+"""
+Merge and update operators to dict  data structure
+"""
+
+user_one = {
+    "name":"Andrew ng",
+    "age":40,
+    "email":"andrewng@domain.com"
+}
+
+user_update = {
+
+  "name":"Andrew",
+    "age":39,
+    "email":"andrew@domain.com",
+    "is_online":True
+}
+
+#new  merge operator
+merge_dict = user_one | user_update
+print(merge_dict)
+
+"""
+Result:
+
+{'name': 'Andrew', 'age': 39, 'email': 'andrew@domain.com', 'is_online': True}
+"""
+
+
+
+#new update operator
+
+user_one |= user_update
+#user_one update
+print(user_one)
+
+"""
+Result:
+{'name': 'Andrew', 'age': 39, 'email': 'andrew@domain.com', 'is_online': True}
+
+"""
--- native_times.py
+++ native_times.py
@@ -1,3 +1,15 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
+"""
+Use of zoneinfo module native to timezone
+"""
+
+current_time = datetime.now()
+#print current time in my time zone
+print(current_time)
+
+current_time_angeles = datetime.now(ZoneInfo("America/Los_Angeles"))
+
+#print  current time in any time zone
+print(current_time_angeles)
