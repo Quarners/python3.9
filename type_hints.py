6c7b0a22832f59184b94cf324829722a80983865
jairoufps <jairoandrescp@ufps.edu.co>
2020-10-11 02:19:14 -0500

    added document functions of type hints

--- type_hints.py
+++ type_hints.py
@@ -1,11 +1,17 @@
 # news type hints native whithout  import  Dict or  List by typing
 
 
-def print_content_list(costumers: list):
+def print_content_list(costumers: list[str]):
+    """
+    Type list[str] allowed whithout use typing module
+    """
     for costumer in costumers:
         print(costumer)
 
 def print_content_dict(costumer:dict):
+    """
+    Type dict allowed whithout use typing module
+    """
     for key_costumer in costumer.keys():
         print(costumer[key_costumer])
 
@@ -17,6 +23,16 @@ print_content_list(costumers=costumers)
 print()
 print("Costumer Dict")
 
+"""
+Result:
+Costumer List
+Mr. Andrew ng
+Jairo Pacheco
+Mr. Angel Diaz
+Carlos Antonio
+
+"""
+
 costumer_dict = {
     "name":"Andrew ng",
     "email":"andrewng@doamin.com",
@@ -24,3 +40,14 @@ costumer_dict = {
 }
 
 print_content_dict(costumer=costumer_dict)
+
+
+"""
+Result:
+
+Costumer Dict
+Andrew ng
+andrewng@doamin.com
+40
+
+"""
